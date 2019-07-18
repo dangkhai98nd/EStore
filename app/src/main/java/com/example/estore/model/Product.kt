4116022ce26619a06ab28e1146d494b614cd5e5a
cgz104 <cuong.le1004@gmail.com>
2019-07-18 13:57:37 +0700

    Update model

--- app/src/main/java/com/example/estore/model/Product.kt
+++ app/src/main/java/com/example/estore/model/Product.kt
@@ -4,18 +4,40 @@ class Product() {
     var id: String? = null
     var name: String? = null
     var price: Int? = null
-    var photo: String? = null
+    var photoDark: String? = null
+    var photoLight: String? = null
     var likeCounter: Int? = null
     var commentCounter: Int? = null
+    var type: String? = null //phone? table? watch? headphones?
+    var brand: String? = null //samsung? apple? oppo? xiaomi? beats? skullcandy?
+    var os: String? = null // android? ios? null?
+    var numberSold: Int? = null
 
     //    var color: List<String>?
-    constructor(id: String?, name: String?, price: Int?, photo: String?, likeCounter: Int?, commentCounter: Int?) : this() {
+    constructor(
+        id: String?,
+        name: String?,
+        price: Int?,
+        photoDark: String?,
+        photoLight: String?,
+        likeCounter: Int?,
+        commentCounter: Int?,
+        type: String?,
+        brand: String?,
+        os: String?,
+        numberSold: Int?
+    ) : this() {
         this.id = id
         this.commentCounter = commentCounter
         this.likeCounter = likeCounter
         this.name = name
-        this.photo = photo
+        this.photoDark = photoDark
+        this.photoLight = photoLight
         this.price = price
+        this.type = type
+        this.brand = brand
+        this.os = os
+        this.numberSold = numberSold
     }
 }
 
--- app/src/main/java/com/example/estore/model/User.kt
+++ app/src/main/java/com/example/estore/model/User.kt
@@ -3,13 +3,9 @@ package com.example.estore.model
 class User() {
     var id: String? = null
     var userName: String? = null
-    var email: String? = null
-    var password: String? = null
 
-    constructor(id: String?, userName: String?, email: String?, password: String?) : this() {
+    constructor(id: String?, userName: String?) : this() {
         this.id = id
         this.userName = userName
-        this.email = email
-        this.password = password
     }
 }
