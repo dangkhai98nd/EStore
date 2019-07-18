90f535e36823ff92b455ff3e8afee1b7428a127e
cgz104 <cuong.le1004@gmail.com>
2019-07-18 15:40:40 +0700

    Update model

--- app/src/main/java/com/example/estore/model/Product.kt
+++ app/src/main/java/com/example/estore/model/Product.kt
@@ -1,6 +1,6 @@
 package com.example.estore.model
 
-class Product() {
+class Product {
     var id: String? = null
     var name: String? = null
     var price: Int? = null
@@ -12,32 +12,9 @@ class Product() {
     var brand: String? = null //samsung? apple? oppo? xiaomi? beats? skullcandy?
     var os: String? = null // android? ios? null?
     var numberSold: Int? = null
+    var trending: Boolean? = null
+    var listUserLike: MutableList<String>? = null
 
-    //    var color: List<String>?
-    constructor(
-        id: String?,
-        name: String?,
-        price: Int?,
-        photoDark: String?,
-        photoLight: String?,
-        likeCounter: Int?,
-        commentCounter: Int?,
-        type: String?,
-        brand: String?,
-        os: String?,
-        numberSold: Int?
-    ) : this() {
-        this.id = id
-        this.commentCounter = commentCounter
-        this.likeCounter = likeCounter
-        this.name = name
-        this.photoDark = photoDark
-        this.photoLight = photoLight
-        this.price = price
-        this.type = type
-        this.brand = brand
-        this.os = os
-        this.numberSold = numberSold
-    }
+
 }
 
--- app/src/main/java/com/example/estore/model/User.kt
+++ app/src/main/java/com/example/estore/model/User.kt
@@ -3,9 +3,13 @@ package com.example.estore.model
 class User() {
     var id: String? = null
     var userName: String? = null
+    var cartList: MutableList<String>? = null
+    var listFavorite: MutableList<String>? = null
 
-    constructor(id: String?, userName: String?) : this() {
+    constructor(id: String?, userName: String?, cartList: MutableList<String>?, listFavorite: MutableList<String>?) : this() {
         this.id = id
         this.userName = userName
+        this.cartList = cartList
+        this.listFavorite = listFavorite
     }
 }
