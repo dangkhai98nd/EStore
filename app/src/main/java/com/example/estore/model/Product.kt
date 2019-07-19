5fea758d2a5bea605f3715b6065ea94921c1cb45
cgz104 <cuong.le1004@gmail.com>
2019-07-19 15:45:28 +0700

    Update model

--- app/src/main/java/com/example/estore/model/Product.kt
+++ app/src/main/java/com/example/estore/model/Product.kt
@@ -1,9 +1,6 @@
 package com.example.estore.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
-class Product() : Parcelable {
+class Product() {
     var id: String? = null
     var name: String? = null
     var price: Int? = null
@@ -18,52 +15,6 @@ class Product() : Parcelable {
     var trending: Boolean? = null
     var listUserLike: MutableList<String>? = null
     var rating: Int? = null
-
-    constructor(parcel: Parcel) : this() {
-        id = parcel.readString()
-        name = parcel.readString()
-        price = parcel.readValue(Int::class.java.classLoader) as? Int
-        photoDark = parcel.readString()
-        photoLight = parcel.readString()
-        likeCounter = parcel.readValue(Int::class.java.classLoader) as? Int
-        commentCounter = parcel.readValue(Int::class.java.classLoader) as? Int
-        type = parcel.readString()
-        brand = parcel.readString()
-        os = parcel.readString()
-        numberSold = parcel.readValue(Int::class.java.classLoader) as? Int
-        trending = parcel.readValue(Boolean::class.java.classLoader) as? Boolean
-        rating = parcel.readValue(Int::class.java.classLoader) as? Int
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(name)
-        parcel.writeValue(price)
-        parcel.writeString(photoDark)
-        parcel.writeString(photoLight)
-        parcel.writeValue(likeCounter)
-        parcel.writeValue(commentCounter)
-        parcel.writeString(type)
-        parcel.writeString(brand)
-        parcel.writeString(os)
-        parcel.writeValue(numberSold)
-        parcel.writeValue(trending)
-        parcel.writeValue(rating)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Product> {
-        override fun createFromParcel(parcel: Parcel): Product {
-            return Product(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Product?> {
-            return arrayOfNulls(size)
-        }
-    }
 
 
 }
