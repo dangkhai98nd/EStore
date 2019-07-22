57b51b4771514f8c4e4bc7aef7a1d203c16e310d
cgz104 <cuong.le1004@gmail.com>
2019-07-22 09:28:01 +0700

    Fix product model

--- app/src/main/java/com/example/estore/model/Product.kt
+++ app/src/main/java/com/example/estore/model/Product.kt
@@ -16,6 +16,39 @@ class Product() {
     var listUserLike: MutableList<String>? = null
     var rating: Int? = null
 
+    constructor(
+        id: String? = null,
+        name: String? = null,
+        price: Int? = null,
+        photoDark: String? = null,
+        photoLight: String? = null,
+        likeCounter: Int? = null,
+        commentCounter: Int? = null,
+        type: String? = null, //phone? table? watch? headphones?
+        brand: String? = null, //samsung? apple? oppo? xiaomi? beats? skullcandy?
+        os: String? = null, // android? ios? null?
+        numberSold: Int? = null,
+        trending: Boolean? = null,
+        listUserLike: MutableList<String>? = null,
+        rating: Int? = null
+    ) : this() {
+
+        this.id = id
+        this.name = name
+        this.price = price
+        this.photoDark = photoDark
+        this.photoLight = photoLight
+        this.likeCounter = likeCounter
+        this.commentCounter = commentCounter
+        this.type = type
+        this.brand = brand
+        this.os = os
+        this.numberSold = numberSold
+        this.trending = trending
+        this.listUserLike = listUserLike
+        this.rating = rating
+    }
+
 
 }
 
