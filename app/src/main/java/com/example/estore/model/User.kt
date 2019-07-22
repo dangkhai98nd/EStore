6e9bd0b730da076ad7f8ab8ba3a705886d8dba68
cgz104 <cuong.le1004@gmail.com>
2019-07-22 09:26:24 +0700

    Update model

--- app/src/main/java/com/example/estore/model/User.kt
+++ app/src/main/java/com/example/estore/model/User.kt
@@ -3,12 +3,19 @@ package com.example.estore.model
 class User() {
     var id: String? = null
     var userName: String? = null
-    var cartList: MutableList<String>? = null
+    var cartList: MutableList<ProductCart>? = null
     var listFavorite: MutableList<String>? = null
     var status: String? = null
     var profilePhoto: String? = null
 
-    constructor(id: String?, userName: String?, cartList: MutableList<String>?, listFavorite: MutableList<String>?, status: String?, profilePhoto: String?) : this() {
+    constructor(
+        id: String?,
+        userName: String?,
+        cartList: MutableList<ProductCart>?,
+        listFavorite: MutableList<String>?,
+        status: String?,
+        profilePhoto: String?
+    ) : this() {
         this.id = id
         this.userName = userName
         this.cartList = cartList
