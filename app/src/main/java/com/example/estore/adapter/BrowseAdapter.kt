8dfc13e21f3b1a1769217897f4562da4a1b9780b
dangkhai98nd <dangkhai98nd@gmail.com>
2019-07-22 17:03:42 +0700

    update BrowseAdapter.kt

--- app/src/main/java/com/example/estore/adapter/BrowseAdapter.kt
+++ app/src/main/java/com/example/estore/adapter/BrowseAdapter.kt
@@ -1,16 +1,20 @@
 package com.example.estore.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.estore.R
 import com.example.estore.model.Product
+import com.example.estore.ui.DetailActivity
 import kotlinx.android.extensions.LayoutContainer
 
 class BrowseAdapter (
@@ -39,6 +43,8 @@ class BrowseAdapter (
         private val tvProductPriceBrowse = containerView.findViewById<TextView>(R.id.tvProductPriceBrowse)
         private val tvLikeCounterBrowse = containerView.findViewById<TextView>(R.id.tvLikeCounterBrowse)
         private val tvCommentCounterBrowse = containerView.findViewById<TextView>(R.id.tvCommentCounterBrowse)
+        private val cvItemBrowse = containerView.findViewById<CardView>(R.id.cvItemBrowse)
+
 
         fun bind(product : Product) {
             Glide.with(mContext)
@@ -48,9 +54,26 @@ class BrowseAdapter (
             if (product.trending == true)
                 clTrendingBrowse.visibility = ConstraintLayout.VISIBLE
             else clTrendingBrowse.visibility = ConstraintLayout.GONE
-            tvProductPriceBrowse.text = "\$" + product?.price.toString()
-            tvLikeCounterBrowse.text = "${product?.likeCounter} likes"
-            tvCommentCounterBrowse.text = "${product?.commentCounter} comments"
+            tvProductNameBrowse.isSelected = true
+            tvProductPriceBrowse.text = "\$" + "${product.price}"
+            tvLikeCounterBrowse.text = "${product.listUserLike?.size} likes"
+            tvCommentCounterBrowse.text = "${product.commentCounter} comments"
+            cvItemBrowse.setOnClickListener {
+                val intent = Intent(mContext,DetailActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                mContext.startActivity(intent)
+            }
         }
+    }
+
+    fun addAll(products : List<Product>) {
+        this.products.addAll(products)
+        notifyDataSetChanged()
+    }
+
+    fun add(product: Product) {
+        Log.e("name","${product.name}")
+        this.products.add(product)
+        notifyDataSetChanged()
     }
 }
