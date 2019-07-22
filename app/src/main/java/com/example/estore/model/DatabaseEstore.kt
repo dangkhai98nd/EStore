f2e70f3c8912df40da6114bf4ce2997e9f17fb4a
dangkhai98nd <dangkhai98nd@gmail.com>
2019-07-22 13:31:45 +0700

    update DatabaseEstore.kt

--- app/src/main/java/com/example/estore/model/DatabaseEstore.kt
+++ app/src/main/java/com/example/estore/model/DatabaseEstore.kt
@@ -1,23 +1,32 @@
 package com.example.estore.model
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import com.example.estore.FirebaseFunction
+import com.google.firebase.database.*
 
 class DatabaseEstore {
     companion object {
         var database : MutableList<Product> = mutableListOf()
-        fun getDatabase(lifecycleOwner: LifecycleOwner) {
+        var userEstore: User? = null
+        private lateinit var databaseRef: DatabaseReference
+        fun getDatabase(lifecycleOwner: LifecycleOwner,idUser : String) {
             val firebaseFunction = FirebaseFunction()
             firebaseFunction.productLiveData.observe(lifecycleOwner , Observer {
                 database.add(it)
 //            Log.e("observe size","${products.size}")
 //            Toast.makeText(view.context,"${products[0].name}",Toast.LENGTH_SHORT).show()
             })
+
+            firebaseFunction.userLiveData.observe(lifecycleOwner, Observer {
+                userEstore = it
+            })
+
             firebaseFunction.estoreGetProductAll()
+            firebaseFunction.estoreGetUserFromId(idUser)
         }
 
-        var userEstore: User? = null
     }
 }
