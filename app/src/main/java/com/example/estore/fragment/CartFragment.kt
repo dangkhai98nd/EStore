b04c0d49d2fabd30ccf828491758195405cbae8f
dangkhai98nd <dangkhai98nd@gmail.com>
2019-07-23 15:00:12 +0700

    fix bug CartFragment.kt

--- app/src/main/java/com/example/estore/fragment/CartFragment.kt
+++ app/src/main/java/com/example/estore/fragment/CartFragment.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,23 +13,29 @@ import com.example.estore.R
 import com.example.estore.adapter.CartAdapter
 import com.example.estore.model.DatabaseEstore
 import kotlinx.android.synthetic.main.fragment_cart.*
+import kotlinx.android.synthetic.main.fragment_cart.view.*
 
 class CartFragment : Fragment() {
     private var cartAdapter : CartAdapter? = null
+    private var subtotal : Int = 0
+    private var root : View? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_cart,container,false)
+        root = inflater.inflate(R.layout.fragment_cart,container,false)
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         setupAdapter(view)
         Handler().postDelayed({
             cartAdapter?.add(DatabaseEstore.userEstore?.cartList)
             Handler().postDelayed({
-                val subtotal = cartAdapter?.getSubtotal()
-                tvSubtotalCart.text = subtotal.toString()
-                tvTaxesCart.text = 0.1f.times(subtotal ?: 0).toString()
-                tvTotalCart.text = 1.1f.times(subtotal ?: 0).toString()
-            },1000)
+                subtotal = cartAdapter?.getSubtotal() ?: 0
+                root?.tvSubtotalCart?.text = 1f.times(subtotal).toString()
+                root?.tvTaxesCart?.text = 0.1f.times(subtotal).toString()
+                root?.tvTotalCart?.text = 1.1f.times(subtotal).toString()
+            },2000)
         },2000)
 
     }
