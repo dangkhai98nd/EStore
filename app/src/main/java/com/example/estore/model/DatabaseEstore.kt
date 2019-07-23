ec62496e04538a69acc8ac78bfcfe4e04617bfe6
cgz104 <cuong.le1004@gmail.com>
2019-07-23 11:07:16 +0700

    Optimize import, fix getDatabase method

--- app/src/main/java/com/example/estore/model/DatabaseEstore.kt
+++ app/src/main/java/com/example/estore/model/DatabaseEstore.kt
@@ -1,21 +1,17 @@
 package com.example.estore.model
 
-import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import com.example.estore.FirebaseFunction
-import com.google.firebase.database.*
 
 class DatabaseEstore {
     companion object {
         var database : MutableList<Product> = mutableListOf()
         var userEstore: User? = null
-        private lateinit var databaseRef: DatabaseReference
-        fun getDatabase(lifecycleOwner: LifecycleOwner,idUser : String) {
+        fun getDatabase(lifecycleOwner: LifecycleOwner, idUser: String ){
             val firebaseFunction = FirebaseFunction()
             firebaseFunction.productLiveData.observe(lifecycleOwner , Observer {
-                database.add(it)
+                database.addAll(it)
 //            Log.e("observe size","${products.size}")
 //            Toast.makeText(view.context,"${products[0].name}",Toast.LENGTH_SHORT).show()
             })
